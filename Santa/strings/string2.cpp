1b3cee5a0e850000d5f9b44ce63fca29122f8f9c
Satalicious <91656114+Satalicious@users.noreply.github.com>
2022-03-21 21:53:55 +0100

    needs fix

--- Santa/strings/string2.cpp
+++ Santa/strings/string2.cpp
@@ -1,5 +1,4 @@
 
-
 /*
 String Functions Implement the following functions:
 
@@ -18,11 +17,10 @@ Name the source file: cstring.c
 
 #include <stdio.h>
 #include <string.h>
-#include <conio.h>
 #include <ctype.h>
 
 void str_inverse_case(char str[]);
-bool str_starts_with(char str[], char prefix[]);
+bool str_starts_with(char str[], char prefix[]); // Return true if str starts with prefix, otherwise false.
 bool str_ends_with(char str[], char suffix[]);  // true if str ends with suffix
 int str_count(char str[], char sub[]);  // number of occurences of sub in text
 bool str_is_ascii(char str[]);  // true if string only has ascii chars
@@ -31,26 +29,30 @@ int str_index(char str[], char sub[]);  // pos. of first occ. of sub in str | -1
 
 void str_inverse_case(char str[]){
     for (int i = 0; i < strlen(str); i++){
-        if (islower(str[i]))
-            toupper(str[i]);
-        else
-            tolower(str[i]);
+        if (islower(str[i]) != 0){
+          str[i] = toupper(str[i]);
+          printf("%c",str[i]);
+          } else {
+            str[i] = tolower(str[i]);
+            printf("%c",str[i]);
+          }
     } 
 }
-bool str_starts_with(char str[], char prefix[]){
-    int count = 0;
-    for (int i = 0; i < strlen(str); i++){
-        for (int j = 0; j < strlen(prefix); j++){
-            if (prefix[j] == str[i])
-                count+=1;
-        }
-    }
-    if (count == strlen(prefix))
-        return true;
-    else
-        return false;
-}
 
+bool str_starts_with(char str[], char prefix[]) {
+	bool isPrefix = false;
+	
+	for (int i = 0; i < strlen(prefix); i++) {
+		if (str[i] == prefix[i]) {
+			isPrefix = true;
+		} else {
+			isPrefix = false;
+		}
+	}
+	
+	return isPrefix;
+}
+/*
 bool str_ends_with(char str[], char suffix[]){
     int count = 0;
     for (int i = 0; i < strlen(str); i++){
@@ -63,7 +65,7 @@ bool str_ends_with(char str[], char suffix[]){
 int str_count(char str[], char sub[]) {
     int occ = 0;
     for (int i = 0; i < strlen(str); i++){
-        if (str[i] == sub)
+        if (str[i] == *sub)
             occ +=1;
     }
     return occ;
@@ -79,12 +81,12 @@ bool str_is_ascii(char str[]) {
 }
 
 bool str_is_digit(char str[]) {
-    int counter = 0;
+    int count = 0;
     for (int i = 0; i < strlen(str); i++) {
         if (str[i] <= 57 || str[i] >= 48)
-            counter+=1
+            count+=1;
     }
-    if (counter == strlen(str))
+    if (count == strlen(str))
         return true;
     else
         return false;
@@ -92,12 +94,43 @@ bool str_is_digit(char str[]) {
 
 int str_index(char str[], char sub[]) {
     for (int i = 0; i < strlen(str); i++) {
-        if (str[i] == sub)
-            return i+1
+        if (str[i] == *sub)
+            return i+1;
     }
 }
+*/
 
 int main(){
-    str_inverse_case("Hello World!");
-    return 0;
+  printf("=== Inverting case in a string ===\n");
+
+  char inverse_case[] = "Hello World!";
+	str_inverse_case(inverse_case);
+  
+  printf("\n=== Checking string's prefix ===\n");
+  char str[] = "This is a text";
+	char substr_1[] = "This is";
+	bool res = str_starts_with(str, substr_1);
+	if (res == true) printf("true\n");
+	else printf("false\n");
+	char substr_2[] = "This is not";
+	res = str_starts_with(str, substr_2);
+	if (res == true) printf("true\n");
+	else printf("false\n");	
+
+  /*
+  str_ends_with("Hello World", "ld");  // true if str ends with suffix
+  str_ends_with("Hello World", "Hehe");
+  
+  str_count("Hello World", "el");  // number of occurences of sub in text
+  str_count("Hello World", "q");
+  
+  str_is_ascii("Hello World");  // true if string only has ascii chars
+  str_is_ascii("_@°");
+  
+  str_is_digit("213454509");  // true if string only has digits
+  str_is_digit("Hello World");
+  
+  str_index("Hello World", "o");  // pos. of first occ. of sub in str | -1
+  */
+  return 0;
 }
