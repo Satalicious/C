0f37074d4cda915d995f3e1e2417d0821dc80b87
Satalicious <91656114+Satalicious@users.noreply.github.com>
2022-04-05 11:20:23 +0200

    newexercise

---
+++ Santa/allgemein/pointers/string_manipulation.cpp
@@ -0,0 +1,30 @@
+/* 1.) Initialize arrays
+    Implement the following functions: 
+
+    int main();
+    void zeros(int* array, int count);
+    void ones(int* array, int count);
+    void range(int* array, int count);
+
+zeros(.) gets an array and the number of its elements. It should set each element to zero. ones(.) does the same as zeros, but sets each value to one. range(.) sets the array to 0, 1, 2, 3, ..., count-1
+Implement main(.) to initizlize an array, call each of the above functions and print the array right after each function call.
+Name the source file: initialize_arrays.c
+
+2. ) String operations
+    Implement the following functions:
+int main();
+void to_upper(char* array);
+bool has_upper(char* array);
+bool is_upper(char* array);
+void to_lower(char* array);
+bool has_lower(char* array);
+bool is_lower(char* array);
+
+*/
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello\n");
+    return 0;
+}
