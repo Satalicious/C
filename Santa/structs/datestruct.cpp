177ef397131f7e18f8bccef66137caa2adb8e731
Satalicious <91656114+Satalicious@users.noreply.github.com>
2022-04-20 11:11:30 +0200

    less zingerle 2004

--- Santa/structs/datestruct.cpp
+++ Santa/structs/datestruct.cpp
@@ -1,19 +1,24 @@
-
 #include <stdio.h>
-
-
-typedef struct {
+typedef struct{ //struct definition
     int year;
     int month;
     int day;
-} Date;
+  } Date;
 
-void print_date(Date d) {
-    printf("My birthday is: %04d-%02d-%02d\n", d.year, d.month, d.day);
+void print_date(Date *datum);//function protoype
+void print_no_pointer(Date datum);
+
+int main(void) {
+  Date datum = {1999, 1, 13};
+  Date* dptr = &datum; // a pointer to pass to the function
+  print_date(dptr);
+  print_no_pointer(datum);
 }
 
-int main() {
-    Date d = { 1997, 06, 16 };
-    print_date(d);
-    return 0;
+void print_date(Date *datum){
+  printf("My date of Birth %d.%d.%d\n", datum->day, datum->month, datum->year); // syntactic sugar to access the struct members
+}
+
+void print_no_pointer(Date datum){
+  printf("My date of Birth without pointers %d.%d.%d\n", datum.day, datum.month, datum.year);
 }
