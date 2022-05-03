08b9756f4871802280c9bdebbacaa03fc133724d
Satalicious <91656114+Satalicious@users.noreply.github.com>
2022-05-03 11:17:21 +0200

    .

---
+++ Santa/Dynamic Memory Management/create_arrays.h
@@ -0,0 +1,7 @@
+
+
+#pragma once
+
+int* zeros(int count);
+int* ones(int count);
+int* range(int count);
---
+++ Santa/Dynamic Memory Management/main.c
@@ -0,0 +1,9 @@
+
+#include "create_arrays.h"
+#include "int_array.h"
+
+int main() {
+
+
+    return 0;
+}
